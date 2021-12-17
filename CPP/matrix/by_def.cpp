1ed47414869a25091ff47871ede75f0b1eecdfe0
Stephen Kelman <stephenrkelman@gmail.com>
2021-12-17 13:46:16 -0800

    by-definition matrix multiplication

---
+++ CPP/matrix/by_def.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+
+using namespace std;
+
+bool valid(vector<int> A, vector<int> B)
+{
+	if(A.size() > 0 && B.size() > 0 &&
+			A[0].size() > 0 && B[0].size() > 0 &&
+			A[0].size() == B.size())
+		return true;
+	else
+		return false;
+}
+
+vector<vector<int>> multiply(vector<int> A, vector<int> B)
+{
+	if(!valid(A,B)) return NULL;
+
+	size_t m = A.size();
+	size_t n = A[0].size();
+	size_t p = B[0].size();
+
+	vector<vector<int>> C(m, vector<int> (p));
+
+	for(int i = 0; i < m; i++)
+	{
+		for(int k = 0; k < p; k++)
+		{
+			for(int j = 0; j < n; j++)
+			{
+				C[i][k] += A[i][j]*B[j][k];
+			}
+		}
+	}
+	return C;
+}
---
+++ Rust/matrix/by_def.rs
@@ -0,0 +1,41 @@
+use std::mem
+use std::vec
+
+fn valid(A: Vec<Vec<i32>>, B: Vec<Vec<i32>>)
+{
+    if A.len() > 0 && B.len() > 0 && A[0].len() > 0 && B[0].len() > 0 && A[0].len() == B.len())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+fn multiply(A: Vec<Vec<i32>>, B: Vec<Vec<i32>>)
+{
+    if !valid(A,B)
+    {
+        return None;
+    }
+
+    let m = A.len();
+    let n = A[0].len();
+    let p = B[0].len();
+
+    let C = vec![vec![0; m]; p]
+
+    for i in 0..m
+    {
+        for k in 0..p
+        {
+            for j in 0..n
+            {
+                C[i][k] += A[i][j]*B[j][k];
+            }
+        }
+    }
+    return C;
+}
